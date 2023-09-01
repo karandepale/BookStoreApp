243ace35af95ee3ce096c358f5f9c9319e875359
“karandepale” <karandepale111@gmail.com>
2023-09-01 16:31:09 +0530

    [Karan] Implement : Implementd the logic for generate Jwt

--- BookStore.Admin/Services/AdminRepo.cs
+++ BookStore.Admin/Services/AdminRepo.cs
@@ -3,20 +3,26 @@ using BookStore.Admin.Entity;
 using BookStore.Admin.Interfaces;
 using BookStore.Admin.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BookStore.Admin.Services
 {
     public class AdminRepo  : IAdminRepo
     {
+        private readonly IConfiguration configuration;
         private readonly AdminContext adminContext;
-        public AdminRepo(AdminContext adminContext)
+        public AdminRepo(AdminContext adminContext, IConfiguration configuration)
         {
             this.adminContext = adminContext;
+            this.configuration = configuration;
         }
 
 
-       //USER REGISTRATION:-
-       public AdminEntity Registration(AdminRegistrationModel model)
+        //USER REGISTRATION:-
+        public AdminEntity Registration(AdminRegistrationModel model)
         {
             try
             {
@@ -42,6 +48,25 @@ namespace BookStore.Admin.Services
                 throw new Exception("Registration Failed Due to some Exception", e);
             }
         }
+
+
+        //GENERATE JWT TOKEN:-
+        public string GenerateJwtToken(string Email, long AdminID)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("AdminID", AdminID.ToString()),
+                new Claim(ClaimTypes.Email, Email),
+                new Claim(ClaimTypes.Role,"Admin")
+        };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(configuration["JwtSettings:Issuer"], configuration["JwtSettings:Audience"], claims, DateTime.Now, DateTime.Now.AddHours(1), creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        
 
 
     }
