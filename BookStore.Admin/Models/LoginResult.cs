df50df9fb6d08d49b0b9f35a5cf560b504d81b84
“karandepale” <karandepale111@gmail.com>
2023-09-01 16:20:22 +0530

    [Karan] Add : Model Added for Login and Response from login api

---
+++ BookStore.Admin/Models/LoginResult.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.Admin.Entity;
+
+namespace BookStore.Admin.Models
+{
+    public class LoginResult
+    {
+        public AdminEntity AdminEntity { get; set; }
+        public string JwtToken { get; set; }
+    }
+}
